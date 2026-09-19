fc2b24334516aa96497f92b49cb2ae2645d933d9
jaeminahn <jamie50240002@gmail.com>
2026-09-19 15:16:59 +0900

    0919 naturalize blink eyelids

--- apps/web/src/pages/MainPage.tsx
+++ apps/web/src/pages/MainPage.tsx
@@ -106,7 +106,7 @@ export function MainPage() {
         <div className="speech-bubble" aria-live="polite">“{speech}”</div>
         <div className="avatar-frame">
           <img className="boss-avatar" src={`/avatars/${boss.avatarKey}.png`} alt={`${boss.alias} 픽셀 아바타`}/>
-          <span className={`avatar-blink avatar-blink--${boss.avatarKey}`} aria-hidden="true"><i/><i/></span>
+          <span className={`avatar-blink avatar-blink--${boss.avatarKey}`} aria-hidden="true"><i><b/></i><i><b/></i></span>
           <span className="thinking-indicator" aria-hidden="true"><i/><i/><i/></span>
         </div>
         <h1 id="boss-alias" className="boss-alias">{boss.alias}</h1>
